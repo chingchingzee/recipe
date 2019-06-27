618ca900a87b9267b0911579e3d8e5941c2050ad
chingching <chingchingzee@gmail.com>
2019-06-27 15:43:11 +0800

    class divided
    Signed-off-by: chingching <chingchingzee@gmail.com>

--- class.cpp
+++ class.cpp
@@ -5,3 +5,21 @@ int classB(void)
 {return 2;}
 int classC(void)
 {return 3;}
+//=================divide into different class==============
+int divide_class(int vol)
+{
+    int class_no=0;
+    switch(vol)
+    {
+        case 1:
+        class_no=classA();
+        break;
+        case 2:
+        class_no=classB();
+        break;
+        case 3:
+        class_no=classC();
+        break;
+    }
+    return 
+}
